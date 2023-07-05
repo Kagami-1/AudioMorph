3ad9516dcf9612beaa8a8a56a6a02eb0c5b0b090
Shoeb Chowdhury <shoebchowdhury311@gmail.com>
2023-07-05 04:57:57 +0200

    fixiing node red and code style

--- audio_switch_windows.py
+++ audio_switch_windows.py
@@ -1,51 +1,67 @@
 import serial
 import subprocess
-from time import sleep, time
+import time
 
-# Function to set default audio output
+SERIAL_PORT = 'COM3'
+SPEAKERS_DEVICE_NAME = 'Speakers'
+HEADPHONE = 'Realtek HD Audio 2nd output'
+NIRCMD_PATH = 'nircmd.exe'
+NIRCMD_COMMAND_TEMPLATE = f'{NIRCMD_PATH} setdefaultsounddevice "{{device_name}}" 1'
+FSR_THRESHOLD = 17000
+SWITCH_INTERVAL = 2
+
 def set_default_audio_output(device_name):
-    command = f'nircmd.exe setdefaultsounddevice "{device_name}" 1'
+    """Sets the default audio output."""
+    command = NIRCMD_COMMAND_TEMPLATE.format(device_name=device_name)
     try:
-        output = subprocess.check_output(command, stderr=subprocess.STDOUT, universal_newlines=True)
-        print(f"Output from nircmd: {output}")
+        output = subprocess.check_output(
+            command, stderr=subprocess.STDOUT, universal_newlines=True)
+        print(f'Output from nircmd: {output}')
         return True
     except subprocess.CalledProcessError as e:
-        print(f"Failed to execute nircmd. Command: {command}, Error: {str(e)}")
+        print(f'Failed to execute nircmd. Command: {command}, Error: {str(e)}')
         return False
 
-# Create a Serial object
-ser = serial.Serial('COM3', 9600)
+def main():
+    """Main function to run the script."""
+    # Create a Serial object
+    ser = serial.Serial(SERIAL_PORT, 9600)
 
-# Initialize timer
-last_switch_time = time()
+    # Initialize timer
+    last_switch_time = time.time()
 
-# Set initial output device
-current_device = 'Realtek HD Audio 2nd output'
+    # Set initial output device
+    current_device = HEADPHONE
 
-while True:
-    if ser.inWaiting() > 0:  # if data is available to read
-        try:
-            data = ser.readline().decode('utf-8').strip()  # read a line, decode to string and strip newline
-            print(data)
-            if "Analog reading = " in data:  # Check if data contains FSR reading
-                fsr_reading = int(data.split("= ")[1])  # Extract the FSR reading from data
-                current_time = time()
-                if fsr_reading > 12000 and current_time - last_switch_time >= 2 and current_device != 'Speakers':
-                    print('FSR Reading is above 15000')
-                    current_device = 'Speakers'
-                    success = set_default_audio_output(current_device)
-                    if success:
-                        print(f"Successfully set '{current_device}' as the default audio output.")
-                        last_switch_time = current_time
-                    else:
-                        print(f"Failed to set '{current_device}' as the default audio output.")
-                elif fsr_reading <= 12000 and current_time - last_switch_time >= 2 and current_device != 'Realtek HD Audio 2nd output':
-                    current_device = 'Realtek HD Audio 2nd output'
-                    success = set_default_audio_output(current_device)
-                    if success:
-                        print(f"Successfully set '{current_device}' as the default audio output.")
-                        last_switch_time = current_time
-                    else:
-                        print(f"Failed to set '{current_device}' as the default audio output.")
-        except Exception as e:
-            print(f"Error decoding data: {e}")
+    while True:
+        if ser.inWaiting() > 0:  # if data is available to read
+            try:
+                data = ser.readline().decode('utf-8').strip()  # read a line, decode to string and strip newline
+                print(data)
+                if 'Analog reading = ' in data:  # Check if data contains FSR reading
+                    fsr_reading = int(data.split('=')[1])  # Extract the FSR reading from data
+                    current_time = time.time()
+                    if (fsr_reading > FSR_THRESHOLD and current_time - last_switch_time >= SWITCH_INTERVAL
+                            and current_device != SPEAKERS_DEVICE_NAME):
+                        current_device = SPEAKERS_DEVICE_NAME
+                        if switch_audio_output_device(current_device, current_time):
+                            last_switch_time = current_time
+                    elif (fsr_reading <= FSR_THRESHOLD and current_time - last_switch_time >= SWITCH_INTERVAL
+                          and current_device != HEADPHONE):
+                        current_device = HEADPHONE
+                        if switch_audio_output_device(current_device, current_time):
+                            last_switch_time = current_time
+            except Exception as e:
+                print(f'Error decoding data: {e}')
+
+def switch_audio_output_device(device_name, current_time):
+    """Switches the audio output device and returns success status."""
+    success = set_default_audio_output(device_name)
+    if success:
+        print(f'Successfully set {device_name} as the default audio output.')
+    else:
+        print(f'Failed to set {device_name} as the default audio output.')
+    return success
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from web_server import connect
 from web_server import sendData
 
 fsrAnalogPin = ADC(Pin(26))  # FSR is connected to GP26
-red_LEDpin = PWM(Pin(3))  # connect Red LED to GP3
-green_LEDpin = PWM(Pin(4))  # connect Green LED to GP4
+red_LEDpin = Pin(3, Pin.OUT)  # connect Red LED to GP3
+green_LEDpin = Pin(4, Pin.OUT)  # connect Green LED to GP4
 
 i2c = I2C(0, sda=Pin(8), scl=Pin(9), freq=400000)  # Adjusted SDA and SCL pins
 oled = SSD1306_I2C(128, 64, i2c)
 
 uart = UART(0, 9600)  # UART setup
 
-def map(x, in_min, in_max, out_min, out_max):
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+#def map(x, in_min, in_max, out_min, out_max):
+#    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 def display_mapping(fsr_reading):
     oled.fill(0)
@@ -34,13 +34,14 @@ if __name__=="__main__":
         uart.write(f'FSR Reading: {fsrReading}\n')
 
         if fsrReading < 1000:  # Adjust this threshold as needed
-            red_LEDpin.duty_u16(65535)  # Red LED fully ON
-            green_LEDpin.duty_u16(0)  # Green LED OFF
+            red_LEDpin.value(1)  # Red LED ON
+            green_LEDpin.value(0)  # Green LED OFF
+            sleep(1)
            
         else:
-            green_LEDpin.duty_u16(65535)  # Green LED fully ON
-            red_LEDpin.duty_u16(0)  # Red LED OFF
-            LEDbrightness = map(fsrReading, 500, 65535, 0, 65535)
+            red_LEDpin.value(0)  # Red LED OFF
+            green_LEDpin.value(1)  # Green LED ON
+            sleep(1)
             
         display_mapping(fsrReading)
 
