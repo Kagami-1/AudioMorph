93a43035cca04bafd3ef52cd4393735ca83f9c4c
Shoeb Chowdhury <shoebchowdhury311@gmail.com>
2023-07-05 03:41:59 +0200

    led bug

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from web_server import connect
 from web_server import sendData
 
 fsrAnalogPin = ADC(Pin(26))  # FSR is connected to GP26
-LEDpin = PWM(Pin(3))  # connect Red LED to GP6
+red_LEDpin = PWM(Pin(3))  # connect Red LED to GP3
+green_LEDpin = PWM(Pin(4))  # connect Green LED to GP4
 
 i2c = I2C(0, sda=Pin(8), scl=Pin(9), freq=400000)  # Adjusted SDA and SCL pins
 oled = SSD1306_I2C(128, 64, i2c)
@@ -24,22 +25,24 @@ def display_mapping(fsr_reading):
 
 if __name__=="__main__":
     print(connect())
-    connect()    
+    connect()
+    sendData(fsrAnalogPin.read_u16())
     while True:
         fsrReading = fsrAnalogPin.read_u16()
         print("Analog reading = ", fsrReading)
 
         uart.write(f'FSR Reading: {fsrReading}\n')
-        
 
         if fsrReading < 1000:  # Adjust this threshold as needed
-            LEDpin.duty_u16(0)
-            sendData("headphone lifted")
+            red_LEDpin.duty_u16(65535)  # Red LED fully ON
+            green_LEDpin.duty_u16(0)  # Green LED OFF
+           
         else:
+            green_LEDpin.duty_u16(65535)  # Green LED fully ON
+            red_LEDpin.duty_u16(0)  # Red LED OFF
             LEDbrightness = map(fsrReading, 500, 65535, 0, 65535)
-            LEDpin.duty_u16(int(LEDbrightness))
-
+            
         display_mapping(fsrReading)
 
         sleep(0.1)
-    
+
